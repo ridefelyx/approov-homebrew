83a4484e4929842c8ae800ccfee287ecda3558cc
bart-kneepkens <bart-kneepkens@users.noreply.github.com>
2022-08-25 16:50:32 +0200

    Remove deprecated unneeded flag

--- approov.rb
+++ approov.rb
@@ -5,8 +5,6 @@ class Approov < Formula
   url "https://github.com/approov/homebrew-cli/releases/download/" + tag + "/approov-" + tag + ".tar.gz"
   sha256  "64979951bd45c57e3118b039fd92e3377199ac7db9361c68cb035a1ce11cc893"
 
-  bottle :unneeded
-
   def install
     on_linux do
       bin.install "Linux/approov"
